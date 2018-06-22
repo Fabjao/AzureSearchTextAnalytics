394783a5cbc5d3d3e59dfb21f60fffb33c060cfe
logonrm <logonrm@fiap.com.br>
2018-06-21 22:26:43 -0300

    Codigo de teste

---
+++ ExAzureSearch/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace ExAzureSearch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://thnetazsearch.search.windows.net
+            //6E74D968A34AD1E6A47C9EEAD75ABD09
+            //mbafiap
+
+            SearchServiceClient client = new SearchServiceClient("thnetazsearch",
+                new SearchCredentials("6E74D968A34AD1E6A47C9EEAD75ABD09"));
+            ISearchIndexClient index = client.Indexes.GetClient("mbafiap");
+            #region Enviar o documento para o azure search
+
+            //var document = new CustomDocument
+            //{
+            //    id = "45240",
+            //    cidade = "Ferraz de Vasconcelos",
+            //    email = "rm45240@fiap.com.br",
+            //    idade = 32,
+            //    nome ="Fábio Gonçalves",
+            //    Endereco = "Rua da alegria, 1000"
+            //};
+            //IndexBatch<CustomDocument> batch = IndexBatch.MergeOrUpload(new List<CustomDocument> { document });
+            //index.Documents.Index(batch);
+            #endregion
+
+            #region Fazer a busca do arquivo no azure search
+            Console.WriteLine("digite um termo para a busca");
+            var termo = Console.ReadLine();
+
+           var response = index.Documents.Search<CustomDocument>(termo, new SearchParameters
+            {
+                IncludeTotalResultCount = true,
+                OrderBy = new List<string>{"nome"}
+            });
+            Console.WriteLine($"{response.Count} documentos encontrados");
+
+            foreach (var item in response.Results)
+            {
+                Console.WriteLine($"{item.Document.nome} {item.Document.email}");
+            }
+            Console.Read();
+
+            #endregion
+        }
+    }
+
+    public class CustomDocument
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        [IsFilterable]
+        [IsSortable]
+        [IsRetrievable(true)]
+        public string id { get; set; }
+
+        [IsFilterable]
+        [IsSortable]
+        [IsRetrievable(true)]
+        [IsSearchable]
+        public string nome { get; set; }
+
+        [IsFilterable]
+        [IsSortable]
+        [IsRetrievable(true)]
+        [IsSearchable]
+        public string email { get; set; }
+
+        [IsFilterable]
+        [IsSortable]
+        [IsRetrievable(true)]
+        public int idade { get; set; }
+
+        [IsFilterable]
+        [IsSortable]
+        [IsRetrievable(true)]
+        [IsFacetable]
+        public string cidade { get; set; }
+
+        [JsonProperty("endereco")]
+        [IsFilterable]
+        [IsSortable]
+        [IsRetrievable(true)]
+        [IsSearchable]
+        public string Endereco { get; set; }
+    }
+}
---
+++ ExemploTestAnalytics/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ExemploTestAnalytics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //text-analytics
+
+            Console.WriteLine("Como foi sua experiência em nossa loja?");
+            string opiniao = Console.ReadLine();
+
+            var doc = new
+            {
+                documents = new[]
+                {
+                    new
+                    {
+                        id = 1,
+                        text = opiniao,
+                        language = "en"
+                    }
+                }
+            };
+
+            var json = JsonConvert.SerializeObject(doc);
+            var http = new HttpClient();
+            //primary key
+            var key = "514ae7b737e24f87a42d472a136e5760";
+
+            http.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+
+            byte[] content = UTF8Encoding.UTF8.GetBytes(json);
+
+            //endpoint
+            var endpoint = "https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.0/languages";
+
+            using (var payload = new ByteArrayContent(content))
+            {
+                payload.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = http.PostAsync(endpoint, payload).Result;
+
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadKey();
+            }
+            
+           
+        }
+    }
+}
